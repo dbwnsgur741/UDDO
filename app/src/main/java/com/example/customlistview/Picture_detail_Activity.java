c7a1078cf89169d60da646632e306f76ca6c7979
dbwnsgur741 <dbwnsgur741@gmail.com>
2019-08-28 08:48:47 +0900

    fix some picture_mission_detail layout

--- app/src/main/java/com/example/customlistview/Picture_detail_Activity.java
+++ app/src/main/java/com/example/customlistview/Picture_detail_Activity.java
@@ -70,38 +70,38 @@ public class Picture_detail_Activity extends AppCompatActivity {
                 textView.setText("팀원이 모두 나오게 단체사진을 찍으시오.");
                 break;
             case 1:
-                textView.setText("사진에 나오는 천우각을 찾아가 팀원들과 함께  만세를 하는 사진을 찍으시오.(천우각이 나오고 팀원들이 만세하는 사진이 나와야 성공 인정)");
+                textView.setText("사진에 나오는 천우각을 찾아가\n팀원들과 함께  만세를 하는 사진을 찍으시오.\n(천우각이 나오고 팀원들이 만세하는\n사진이 나와야 성공 인정)");
                 imageView.setImageResource(R.drawable.img31);
                 break;
             case 2:
-                textView.setText("아래에 나오는 사진은 옥인동 윤씨가옥의 내부 사진이다. 사진에 나오는 배경과 똑같은 곳에서 손가락으로 하트 만들어서 사진찍기\n");
+                textView.setText("아래에 나오는 사진은 옥인동 윤씨가옥의 내부 사진이다.\n사진에 나오는 배경과 똑같은 곳에서 손가락으로 하트 만들어서 사진찍기");
                 imageView.setImageResource(R.drawable.img32);
                 break;
             case 3:
-                textView.setText("아래의 사진에 나오는 사진의 가옥은 재기동 해풍부원군 윤택영 재실이다. 해당 안내판을 찾아가서 안내판 읽는 팀원의 사진을 찍으시오.\n");
+                textView.setText("아래의 사진에 나오는 사진의 가옥은\n재기동 해풍부원군 윤택영 재실이다.\n해당 안내판을 찾아가서 안내판 읽는 팀원의 사진을 찍으시오.");
                 imageView.setImageResource(R.drawable.img33);
                 break;
             case 4:
-                textView.setText("보이는 사진은 관훈동 민씨 가옥이다. 관훈동 민씨 가옥 마당에서 팀원끼리 닭싸움 하는 사진을 찍으시오. ");
+                textView.setText("보이는 사진은 관훈동 민씨 가옥이다.\n관훈동 민씨 가옥 마당에서 팀원끼리\n닭싸움 하는 사진을 찍으시오.");
                 imageView.setImageResource(R.drawable.img34);
                 break;
             case 5:
-                textView.setText("삼선동 오위장 김춘영의 가옥을 찾아가서 구경하던 관람객과 같이 기념사진을 찍어라.");
+                textView.setText("삼선동 오위장 김춘영의 가옥을 찾아가서\n구경하던 관람객과 같이 기념사진을 찍어라.");
                 imageView.setImageResource(R.drawable.img35);
                 break;
             case 6:
-                textView.setText("삼각동 도편수 이승엽 가옥을 찾아서 안채 내부가 나오도록 사진찍기 ");
+                textView.setText("삼각동 도편수 이승엽 가옥을 찾아서\n안채 내부가 나오도록 사진찍기 ");
                 imageView.setImageResource(R.drawable.img36);
                 break;
             case 7:
-                textView.setText("타임캡슐 광장에서 팀원들과 함께 제일 자신있는 포즈로 사진찍기");
+                textView.setText("타임캡슐 광장에서 팀원들과 함께\n제일 자신있는 포즈로 사진찍기");
                 imageView.setImageResource(R.drawable.img36);
                 break;
             case 8:
                 textView.setText("한복입은 외국인과 함께 사진 찍기");
                 break;
             case 9:
-                textView.setText("팀원들과 함께 점프샷 찍기(발이 땅에 닿으면 실패!)");
+                textView.setText("팀원들과 함께 점프샷 찍기\n(발이 땅에 닿으면 실패!)");
                 break;
         }
         button = (AppCompatButton) findViewById( R.id.picture_mission_detail_btn );
